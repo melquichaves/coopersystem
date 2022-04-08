eff6a4f4ad8616e2350ad389c06620950e844d48
Melqui Chaves <tp.melquizedeque.ch@totvspartners.com.br>
2022-04-08 19:46:50 -0300

    add gateways strategy

---
+++ src/app/ui/repositories/investment.repository.ts
@@ -0,0 +1,25 @@
+import { Injectable } from "@angular/core";
+import { RemoteGatewayFactory } from "../gateways/remote.gateway.factory";
+import { BackendUrl } from "../models/backend-url";
+import { InvestmentResponse } from "../models/domains/investment.response.model";
+import { InvestmentsParser } from "./parsers/investments/investments.parser";
+
+@Injectable()
+export class InvestmentRepository {
+
+    constructor(
+        private remoteGatewayFactory: RemoteGatewayFactory, 
+        private investmentsParser: InvestmentsParser
+    ) {}
+        
+    public async getInvestments(): Promise<InvestmentResponse> {
+        try {
+            const remoteGateway = this.remoteGatewayFactory.createDefaultRemoteGateway();
+            const data = await remoteGateway.get(new BackendUrl('/api/releasenotes'));
+            return this.investmentsParser.parse(data);
+        }
+        catch (error) {
+            return await Promise.reject(error);
+        }
+    }
+}
